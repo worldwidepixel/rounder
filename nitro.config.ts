43675704f185a01bef527cab0883900c28a50c0a
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-06-15 16:55:53 -0700

    Just going to use vercel until this issue is fixed.

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,4 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  preset: "netlify",
-  output: {
-    dir: ".output",
-    serverDir: ".output/server",
-    publicDir: ".output/public",
-  },
 });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,23 +1,26 @@
 import sharp from "sharp";
 
-export default eventHandler(async (event) => {
-  const query = getQuery(event);
-  const imageUrl = query.url;
-  const image = await fetch(imageUrl.toString());
-  const imageBuffer = await image.arrayBuffer();
-  const radius = query.r;
+export default defineCachedEventHandler(
+  async (event) => {
+    const query = getQuery(event);
+    const imageUrl = query.url;
+    const image = await fetch(imageUrl.toString());
+    const imageBuffer = await image.arrayBuffer();
+    const radius = query.r;
 
-  setHeader(event, "Content-Type", "image/png");
-  let baseImage = sharp(imageBuffer).png().blur(10);
-  const baseMetadata = await baseImage.metadata();
+    setHeader(event, "Content-Type", "image/png");
+    let baseImage = sharp(imageBuffer).png().blur(10);
+    const baseMetadata = await baseImage.metadata();
 
-  const negative = Buffer.from(
-    `<svg><rect x="0" y="0" width="${baseMetadata.width}" height="${baseMetadata.height}" rx="${radius}" ry="50"/></svg>`
-  );
+    const negative = Buffer.from(
+      `<svg><rect x="0" y="0" width="${baseMetadata.width}" height="${baseMetadata.height}" rx="${radius}" ry="50"/></svg>`
+    );
 
-  const roundedImage = sharp(imageBuffer)
-    .png()
-    .composite([{ input: negative, blend: "dest-in" }]);
+    const roundedImage = sharp(imageBuffer)
+      .png()
+      .composite([{ input: negative, blend: "dest-in" }]);
 
-  return roundedImage;
-});
+    return roundedImage;
+  },
+  { maxAge: 60 * 60 * 24 }
+);
